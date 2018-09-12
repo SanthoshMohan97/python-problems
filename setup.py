ce1aa1e821d2e9bb141f6dd65b488ccf153243e3
SanthoshMohan97 <santhoshmohan0897@gmail.com>
2018-09-12 18:06:11 +0530

    PHC_project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PHC management system',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='Santhosh',
+    author_email='santhoshmohan0897@gmail.com',
+    description='HospitalManagementSystem for college python course'
+)
